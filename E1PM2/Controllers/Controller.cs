f775d877496828494b0372bc3db80bcd325b8e50
Daniela Reyes <reyesdaniela498@gmail.com>
2024-02-15 21:51:26 -0600

    1

---
+++ E1PM2/Controllers/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace E1PM2.Controllers
+{
+    public class Controller
+    {
+        readonly SQLiteAsyncConnection _connection;
+
+
+    }
+}
